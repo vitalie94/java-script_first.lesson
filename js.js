185a0c256ca82192226a7d9403df83ee342ab560
Пользователь <you@example.com>
2022-10-20 20:30:35 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,45 @@
+// const name =1;
+// let  name1 ="string";
+// let name2=true;
+// let name3=false;
+// let name4=null;
+// let name5 =undefined;
+// // var name2=3;
+
+
+// // name = "salut";
+// name1 = "salut";
+
+
+
+
+
+
+// console.log(name);
+// console.log(name1)
+
+
+// let score = null;
+// console.log(typeof score);
+
+// let ion = "2" ;
+// let vasea = 4 ;
+// let vasea2 = 7;
+// score = ion + vasea ;
+
+// console.log(typeof ion);
+// console.log(typeof vasea2);
+// const nume = "Mosneag";
+// const prenume = "Vitalie";
+// console.log(nume+ "   "  +prenume);
+
+// console.log(score);
+//  alert("salut");
+//  prompt("salut, cum te numesti?")
+let number = true;
+let zece=10;
+console.log(zece);
+zece = 50
+let score = number + zece 
+console.log(zece);
+console.log(number);
